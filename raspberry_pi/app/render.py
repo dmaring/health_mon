0fbb6efd350f3cf0b8a4c10dab577f0bfe14b152
Andrew Maring <andrew.maring@gmail.com>
2021-06-11 13:49:08 -0700

    Edit render.py to accept args

--- raspberry_pi/app/render.py
+++ raspberry_pi/app/render.py
@@ -15,30 +15,31 @@ import traceback
 
 logging.basicConfig(level=logging.DEBUG)
 
-try:
-    logging.info("epd7in5_V2 Demo")
-    epd = epd7in5_V2.EPD()
+def render(file_name):
+    try:
+        logging.info("Starting rendering...")
+        epd = epd7in5_V2.EPD()
     
-    logging.info("init and Clear")
-    epd.init()
-    epd.Clear()
+        logging.info("init and Clear")
+        epd.init()
+        epd.Clear()
 
-    logging.info("3.read bmp file")
-    Himage = Image.open(os.path.join(picdir, 'latest.jpeg'))
-    epd.display(epd.getbuffer(Himage))
-    time.sleep(2)
+        logging.info("Reading file..")
+        Himage = Image.open(file_name)
+#        Himage = Image.open(os.path.join(picdir, 'latest.jpeg'))
+        epd.display(epd.getbuffer(Himage))
+        time.sleep(2)
 
-#    logging.info("Clear...")
-#    epd.init()
-#    epd.Clear()
-
-    logging.info("Goto Sleep...")
-    epd.sleep()
+        logging.info("Goto Sleep...")
+        epd.sleep()
     
-except IOError as e:
-    logging.info(e)
+    except IOError as e:
+        logging.info(e)
     
-except KeyboardInterrupt:    
-    logging.info("ctrl + c:")
-    epd7in5_V2.epdconfig.module_exit()
-    exit()
+    except KeyboardInterrupt:    
+        logging.info("ctrl + c:")
+        epd7in5_V2.epdconfig.module_exit()
+        exit()
+
+if __name__ == "__main__":
+    render(sys.argv[1])
