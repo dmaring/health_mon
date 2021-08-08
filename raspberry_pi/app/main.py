92fe67fae57f2d70200ab0e3619124865d06c867
Drew Maring <drewmaring@google.com>
2021-08-07 21:56:28 -0600

    Add while True loop

--- raspberry_pi/app/main.py
+++ raspberry_pi/app/main.py
@@ -38,11 +38,12 @@ streaming_pull_future = subscriber.subscribe(subscription_path, callback=callbac
 print(f"Listening for messages on {subscription_path}..\n")
 
 # Wrap subscriber in a 'with' block to automatically call close() when done.
-with subscriber:
-    try:
-        # When `timeout` is not set, result() will block indefinitely,
-        # unless an exception is encountered first.
-        streaming_pull_future.result(timeout=timeout)
-    except TimeoutError:
-        streaming_pull_future.cancel()  # Trigger the shutdown.
-        streaming_pull_future.result()  # Block until the shutdown is complete.
+while True:
+    with subscriber:
+        try:
+            # When `timeout` is not set, result() will block indefinitely,
+            # unless an exception is encountered first.
+            streaming_pull_future.result(timeout=timeout)
+        except TimeoutError:
+            streaming_pull_future.cancel()  # Trigger the shutdown.
+            streaming_pull_future.result()  # Block until the shutdown is complete.
