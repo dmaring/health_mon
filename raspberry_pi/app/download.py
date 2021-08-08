b8102f4036d9dbc3a49f259720cd05f5c36133e0
Drew Maring <drewmaring@google.com>
2021-08-07 21:26:22 -0600

    Add pubsub function to main

--- raspberry_pi/app/download.py
+++ raspberry_pi/app/download.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-# Main application
+# Download file and return path of file
 
 import storage
 import render
 import sys
 
-def main(bucket_name, source_blob_name, destination_file_name):
+def download(bucket_name, source_blob_name, destination_file_name):
 
-    # Check Pub/Sub for new message
-    # Parse message for file name
-
+    
     # Download message fom Cloud Storage
     storage.download_blob(bucket_name, source_blob_name, destination_file_name)
 
     # Render image
-    render.render(destination_file_name)
+    return(destination_file_name)
 
 
 if __name__ == "__main__":
-    main(sys.argv[1], sys.argv[2], sys.argv[3])
+    download(sys.argv[1], sys.argv[2], sys.argv[3])
---
+++ raspberry_pi/app/main.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+# Main script
+
+import storage
+import render
+import sys
+import download
+from concurrent.futures import TimeoutError
+from google.cloud import pubsub_v1
+
+project_id = "drews-project-279817"
+subscription_id = "image_topic-sub"
+# Set location for downloaded file to raspberry pi
+destination_file_name = "/home/pi/app/health_mon/raspberry_pi/images/new"
+# Number of seconds the subscriber should listen for messages
+timeout = 5.0
+
+subscriber = pubsub_v1.SubscriberClient()
+# The `subscription_path` method creates a fully qualified identifier
+# in the form `projects/{project_id}/subscriptions/{subscription_id}`
+subscription_path = subscriber.subscription_path(project_id, subscription_id)
+
+def callback(message):
+    print(f"Received {message}.")
+    message.ack()
+
+    # parse message for bucket_name, source_blob_name
+    bucket_name = message.bucket
+    source_blob_name = message.name
+
+    download(bucket_name, source_blob_name, destination_file_name)
+
+    render(destination_file_name)
+
+
+
+streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
+print(f"Listening for messages on {subscription_path}..\n")
+
+# Wrap subscriber in a 'with' block to automatically call close() when done.
+with subscriber:
+    try:
+        # When `timeout` is not set, result() will block indefinitely,
+        # unless an exception is encountered first.
+        streaming_pull_future.result(timeout=timeout)
+    except TimeoutError:
+        streaming_pull_future.cancel()  # Trigger the shutdown.
+        streaming_pull_future.result()  # Block until the shutdown is complete.
